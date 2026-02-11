402404acd257aba8749491c5adf0485504079ed0
Naman <namansingla830@gmail.com>
2026-02-11 22:33:14 +0530

    authController created Exisiting user

---
+++ backend/controllers/authController.js
@@ -0,0 +1,28 @@
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import userModel from '../models/userModel.js';
+
+export const register = async(req, res) => {
+    const {name, email, password} = req.body;
+
+    if(!name || !email || !password){
+        return res.json({success: false, message: 'Missing Deatils'})
+
+    }
+
+    try {
+        
+        const existingUser = await userModel.findOne({email})
+
+        if (existingUser) {
+            return res.json({success: false, message: "User Already Exist"})
+        }
+
+
+        const hasedPassword = await bcrypt.hash(password, 10);
+
+
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
