ef25c216afee217f94259e6fbb587c4785be1bda
Naman <namansingla830@gmail.com>
2026-02-11 22:42:47 +0530

    userRegister function is created

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 
-export const register = async (req, res) => {
+export const userRegister = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
@@ -18,8 +18,19 @@ export const register = async (req, res) => {
 
     const hasedPassword = await bcrypt.hash(password, 10);
 
-    const user = new userModel({name, email , password: hasedPassword})
-    await user.save()
+    const user = new userModel({ name, email, password: hasedPassword });
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
