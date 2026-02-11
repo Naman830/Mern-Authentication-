31411e250023ebe5dfb50a3ecf28734a9b31b0a9
Naman <namansingla830@gmail.com>
2026-02-11 22:34:58 +0530

    hased password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,28 +1,26 @@
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import userModel from '../models/userModel.js';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
-export const register = async(req, res) => {
-    const {name, email, password} = req.body;
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
 
-    if(!name || !email || !password){
-        return res.json({success: false, message: 'Missing Deatils'})
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: "Missing Deatils" });
+  }
 
+  try {
+    const existingUser = await userModel.findOne({ email });
+
+    if (existingUser) {
+      return res.json({ success: false, message: "User Already Exist" });
     }
 
-    try {
-        
-        const existingUser = await userModel.findOne({email})
+    const hasedPassword = await bcrypt.hash(password, 10);
 
-        if (existingUser) {
-            return res.json({success: false, message: "User Already Exist"})
-        }
-
-
-        const hasedPassword = await bcrypt.hash(password, 10);
-
-
-    } catch (error) {
-        res.json({success: false, message: error.message})
-    }
-}
+    const user = new userModel({name, email , password: hasedPassword})
+    await user.save()
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
