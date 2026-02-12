1c47da02187f699dfb45c6635f05fe1e25cd94da
Naman <namansingla830@gmail.com>
2026-02-13 00:10:53 +0530

    authRoutes created

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { userLogin, userLogout, userRegister } from '../controllers/authController.js'
+
+const authRouter = express.Router()
+
+authRouter.post('/register', userRegister);
+authRouter.post('/login', userLogin);
+authRouter.post('/logout', userLogout);
