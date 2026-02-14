2a4a1251564215c3fbc72d50d1006423ca3a60e4
Naman <namansingla830@gmail.com>
2026-02-14 11:49:12 +0530

    add state in name

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -24,15 +24,18 @@ const Login = () => {
 
         <form>
           {/* Name */}
-          <div className="mb-4 flex items-center gap-3 w-full px-5  py-2.5 rounded-full bg-[#333A5C]">
-            <img src={assets.person_icon} alt="" />
-            <input
-              type="text"
-              placeholder="Full Name"
-              required
-              className="bg-transparent outline-none "
-            />
-          </div>
+          {state === "Sign Up" && (
+            <div className="mb-4 flex items-center gap-3 w-full px-5  py-2.5 rounded-full bg-[#333A5C]">
+              <img src={assets.person_icon} alt="" />
+              <input
+                type="text"
+                placeholder="Full Name"
+                required
+                className="bg-transparent outline-none "
+              />
+            </div>
+          )}
+
           {/* Email */}
           <div className="mb-4 flex items-center gap-3 w-full px-5  py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.mail_icon} alt="" />
@@ -65,11 +68,17 @@ const Login = () => {
         </form>
 
         <p className="text-gray-400 text-center text-xs mt-4 ">
-          Already have an Account <span className="text-blue-400 cursor-pointer underline">Login here</span>
+          Already have an Account{" "}
+          <span className="text-blue-400 cursor-pointer underline">
+            Login here
+          </span>
         </p>
 
-           <p className="text-gray-400 text-center text-xs mt-4 ">
-          Don't have an Account <span className="text-blue-400 cursor-pointer underline">Sign up</span>
+        <p className="text-gray-400 text-center text-xs mt-4 ">
+          Don't have an Account{" "}
+          <span className="text-blue-400 cursor-pointer underline">
+            Sign up
+          </span>
         </p>
       </div>
     </div>
